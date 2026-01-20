018989494b2f22075848429f281d1adf940b19b2
Nurken <nurken.serdakov.2000@mail.ru>
2026-01-20 09:46:05 +0500

    Связь фронтенда с бэкендом

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+
+const Home: React.FC = () => {
+
+  const [message, setMessage] = useState('Загрузка...');
+
+  useEffect(() => {
+    
+    fetch('http://localhost:3000')
+      .then(res => res.text()) 
+      .then(data => {
+        setMessage(data); 
+      })
+      .catch(err => {
+        console.error(err);
+        setMessage('Ошибка: Бэкенд не отвечает');
+      });
+  }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color="primary">
+          <IonTitle>Изучение Китайского</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Статус системы:</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            {}
+            <h2>{message}</h2>
+          </IonCardContent>
+        </IonCard>
+
+        <p>Если Hello World , то тогда работает хехе</p>
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
